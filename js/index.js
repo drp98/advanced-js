ddb48c9231e1ca18fe178252f90875efa3e45f16
Roman Dziadyk <dziadyk.roman@gmail.com>
2020-10-03 04:06:19 +0300

    code refactoring

--- js/index.js
+++ js/index.js
@@ -4,39 +4,37 @@ const cart = {
 }
 
 const getPriceInt = price => parseInt(price.match(/\d+/g).toString());//получение стоимости из строки типа "25 грн."
+const regexpEmail = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
-const productQuantityCart = document.querySelector('#cart-product-quantity');//верхний блок: количество
-const productPriceCart = document.querySelector('#cart-product-price');//верхний блок: цена
+const productQuantityCart = document.querySelector('#cart-product-quantity');
+const productPriceCart = document.querySelector('#cart-product-price');
+const buttonOrder = document.querySelector('#btn-order');
+
+const categorySelect = document.querySelector('#select-category');
+const priceSelect = document.querySelector('#select-price');
+const formOrder = document.querySelector('#form-order');
+const warningBox = document.querySelector('.warning-box');
+
+const nodeListOfQuantityInputs = document.querySelectorAll('.qty__item');
+const nodeListOfProductBoxButtons = document.querySelectorAll('.product-box__btn');
+const nodeListOfProductBox = document.querySelectorAll('.product-box__item');
+
+const modal = document.querySelector('#modal-order');
+const closeModal = document.getElementsByClassName('close')[0];
+const inputEmail = document.querySelector('#email');
+const inputName = document.querySelector('#name');
+const buttonSend = document.querySelector('#button-send');
 
 function renderTopCart({price, quantity}) {
     productPriceCart.innerHTML = price;
     productQuantityCart.innerHTML = quantity;
 }
 
-document.querySelectorAll('.qty__item').forEach(nodeElement => {//validation of inputs
-    nodeElement.addEventListener('change', () => {
-        if(nodeElement.value < 1)
-            nodeElement.value = 1
-    })
-})
-
-document.querySelectorAll('.product-box__btn').forEach(nodeElement => {//listener of add button
-    nodeElement.addEventListener('click', () => {
-        const currentPrice = nodeElement.parentElement.querySelector('p').innerHTML;
-        const currentQuantity = parseInt(nodeElement.parentElement.querySelector('input').value);
-
-        cart.quantity += currentQuantity || 1;
-        cart.price += getPriceInt(currentPrice) * (currentQuantity || 1);
-
-        renderTopCart(cart);
-    })
-})
-
 function sort() {
-    const selectedCategory = document.querySelector('#select-category').value;
-    const selectedPrice = getPriceInt(document.querySelector('#select-price').value);
+    const selectedCategory = categorySelect.value;
+    const selectedPrice = getPriceInt(priceSelect.value);
 
-    document.querySelectorAll('.product-box__item').forEach(item => {
+    nodeListOfProductBox.forEach(item => {
         const productCategory = item.getAttribute('category');
         const productPrice = getPriceInt(item.querySelector('p').innerHTML);
         
@@ -46,60 +44,24 @@ function sort() {
             item.style.display = 'none'
         }
     })
-
 }
 
-function productNotAvailable() {//вывод сообщения об отсутствии товаров
-    const div_list = document.querySelectorAll('.product-box__item'); // returns NodeList
-    const div_array = [...div_list]; // convert NodeList to Array
-    const warningBox = document.querySelector('.warning-box');
-
-    if(div_array.every(item => item.style.display == 'none')) {
+function productNotAvailable() {
+    const arrayOfProductBox = [...nodeListOfProductBox];
+    
+    if(arrayOfProductBox.every(item => item.style.display == 'none')) {
         warningBox.style.display = 'flex';
     } else {
         warningBox.style.display = 'none';
     }
 }
 
-document.querySelector('#select-category').addEventListener('change', sort)
-document.querySelector('#select-price').addEventListener('change', sort)
-document.querySelector('#select-category').addEventListener('change', productNotAvailable)
-document.querySelector('#select-price').addEventListener('change', productNotAvailable)
-
-const buttonOrder = document.querySelector('#btn-order');//кнопка "оформить заказ"
-const modal = document.querySelector('#modal-order');//модальное окно
-const closeModal = document.getElementsByClassName('close')[0];// Get the <span> element that closes the modal
-const buttonSend = document.querySelector('#button-send'); //кнопка "отправить" в модальном окне
-const formOrder = document.querySelector('#form-order');//форма
-
-// When the user clicks the button, open the modal 
-buttonOrder.addEventListener('click', () => {
-    modal.style.display = "block";
-})
-
-// When the user clicks on <span> (x), close the modal
-closeModal.addEventListener('click', () => {
-    modal.style.display = "none";
-})
-
-// When the user clicks anywhere outside of the modal, close it
-window.addEventListener('click', event => {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-})
-
 function validateForm(event) {
     event.preventDefault();
-    const inputEmail = document.querySelector('#email');
-    const inputName = document.querySelector('#name');
-
-    const regexpEmail = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
 
     if(inputEmail.value == '' || inputName.value == '') {
         alert('Поля заполнены неверно');
-        console.log('error');
-    } else if(regexpEmail.test(inputEmail.value) == false) {
+    } else if(!regexpEmail.test(inputEmail.value)) {
         alert('Некорректный email');
     } else {
         modal.style.display = "none";
@@ -114,5 +76,42 @@ function clearCart () {
     cart.price = cart.quantity = 0;
 }
 
-buttonSend.addEventListener('click', validateForm);
+nodeListOfQuantityInputs.forEach(nodeElement => {//validation of inputs
+    nodeElement.addEventListener('change', () => {
+        if(nodeElement.value < 1)
+            nodeElement.value = 1
+    })
+})
 
+nodeListOfProductBoxButtons.forEach(nodeElement => {
+    nodeElement.addEventListener('click', () => {
+        const currentPrice = nodeElement.parentElement.querySelector('p').innerHTML;
+        const currentQuantity = parseInt(nodeElement.parentElement.querySelector('input').value);
+
+        cart.quantity += currentQuantity || 1;
+        cart.price += getPriceInt(currentPrice) * (currentQuantity || 1);
+
+        renderTopCart(cart);
+    })
+})
+
+buttonOrder.addEventListener('click', () => {//open the modal 
+    modal.style.display = "block";
+})
+
+
+closeModal.addEventListener('click', () => {// clicks on <span> (x), close the modal
+    modal.style.display = "none";
+})
+
+window.addEventListener('click', event => {// clicks anywhere outside of the modal, close it
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+})
+
+buttonSend.addEventListener('click', validateForm);
+categorySelect.addEventListener('change', sort);
+categorySelect.addEventListener('change', productNotAvailable);
+priceSelect.addEventListener('change', sort);
+priceSelect.addEventListener('change', productNotAvailable);
