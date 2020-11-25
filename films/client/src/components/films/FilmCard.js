1c0e18fc43228319d54d9c04f23f948b06810637
Roman Dziadyk <dziadyk.roman@gmail.com>
2020-11-25 17:58:20 +0200

    films task-1

--- films/client/src/components/films/FilmCard.js
+++ films/client/src/components/films/FilmCard.js
@@ -1,32 +1,56 @@
-import React from "react"
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import Featured from './Featured'
 
-const FilmCard = ({film}) => (
-    <div className="ui card">
-        <span className="ui right corner label">
-            <i className="empty star icon" />
-        </span>
-        <div className="image">
-            <span className="ui green label ribbon">$ {film?.price} </span>
-            <Featured featured={film.featured} id={film._id} />
-            <img src={film?.img} alt={film?.title} />
-        </div>
+const FilmCard = ({ film }) => {
+    const [toggleDescription, setToggleDescription] = useState(false)
 
-        <div className="content">
-            <span href="#" className="header">
-                {film?.title}
-            </span>
-            <div className="meta">
-                <i className="icon users" /> {film?.director}
-                <span className="right floated">
-                    <i className="icon wait right" />
-                    {film?.duration} min
+    const toggle = () => {
+        setToggleDescription(() => !toggleDescription)
+    }
+
+    return (
+        <div className='ui card'>
+            {!toggleDescription ? (
+                <>
+                    <span className='ui right corner label'>
+                        <i className='empty star icon' />
+                    </span>
+                    <div className='image'>
+                        <span className='ui green label ribbon'>
+                            $ {film?.price}{' '}
+                        </span>
+                        <Featured featured={film.featured} id={film._id} />
+                        <img src={film?.img} alt={film?.title} />
+                    </div>
+                </>
+            ) : (
+                <p>{film.description}</p>
+            )}
+
+            <div className='content'>
+                <span href='#' className='header'>
+                    {film?.title}
                 </span>
+                <div className='meta'>
+                    <i className='icon users' /> {film?.director}
+                    <span className='right floated'>
+                        <i className='icon wait right' />
+                        {film?.duration} min
+                    </span>
+                </div>
+                <i
+                    className={
+                        toggleDescription
+                            ? 'icon link eye slash'
+                            : 'icon link eye'
+                    }
+                    onClick={() => toggle()}
+                />
             </div>
         </div>
-    </div>
-)
+    )
+}
 
 FilmCard.propTypes = {
     film: PropTypes.shape({
@@ -36,7 +60,7 @@ FilmCard.propTypes = {
         duration: PropTypes.number.isRequired,
         price: PropTypes.number.isRequired,
         featured: PropTypes.bool.isRequired,
-    })
+    }),
 }
 
 export default React.memo(FilmCard)
