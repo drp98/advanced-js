6657efd09127fdd9c346a24a6abbbb55fce73857
Roman Dziadyk <dziadyk.roman@gmail.com>
2020-12-19 17:04:40 +0200

    books project initial commit

---
+++ redux/books_project/src/components/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { Route } from 'react-router-dom'
+import Menu from './Menu'
+import { AsyncLoad, lazyLoad } from './AsyncLoad'
+
+const BooksPage = AsyncLoad(lazyLoad('./books/BooksPage'))
+const BookForm = AsyncLoad(lazyLoad('./books/BookForm'))
+
+const App = () => <div className="container">
+    <div className={'container'}>
+        <Menu />
+        <div className={'row'}>
+            <Route exact path='/' component={BooksPage}/>
+            <Route path='/add-book' component={BookForm}/>
+        </div>
+    </div>
+</div>
+
+export default App
